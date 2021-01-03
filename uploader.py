685ea3037daf76396dc5f8720ab3aee9e1c88f7e
Térence Chateigné <terence.chateigne@posteo.net>
2021-01-03 23:25:07 +0100

    domain ->  url

--- uploader.py
+++ uploader.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
-    oc = owncloud.Client(os.getenv("DOMAIN"))
+    oc = owncloud.Client(os.getenv("URL"))
     oc.login(os.getenv("USERNAME"), os.getenv("PASSWORD"))
     for arg in sys.argv[1:]:
         oc.put_file(f"{os.getenv('UPLOAD_DIRECTORY')}/{ntpath.basename(arg)}", arg)
